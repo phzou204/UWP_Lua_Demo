585f8eb1d3cbf298b5798aa6239d00650bed2d94
ph <threshold2015@outlook.com>
2016-06-29 19:45:27 +0800

    add file

---
+++ ToolsLib/StreamConvert.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+
+namespace ToolsLib
+{
+    public class StreamConvert
+    {
+        /// <summary>
+        /// MemoryStream转IRandomAccessStream
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <returns></returns>
+        public static async Task<IRandomAccessStream> ConvertAsync(MemoryStream stream)
+        {
+            var randomAccessStream = new InMemoryRandomAccessStream();
+            var outputStream = randomAccessStream.GetOutputStreamAt(0);
+            var dw = new DataWriter(outputStream);
+            var task = new Task(() => dw.WriteBytes(stream.ToArray()));
+            task.Start();
+            await task;
+            await dw.StoreAsync();
+            var success = await outputStream.FlushAsync();
+            return randomAccessStream;
+        }
+    }
+}
---
+++ ToolsLib/Utility.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace ToolsLib
+{
+    public class Utility
+    {
+        /// <summary>
+        /// 从字节数组获取BitmapImage
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static async Task<BitmapImage> GetBitmapImageAsync(byte[] bytes)
+        {
+            BitmapImage bitmapImage = null;
+            try
+            {
+                using (var ms = new MemoryStream(bytes))
+                {
+                    IRandomAccessStream randomAccessStream = await ToolsLib.StreamConvert.ConvertAsync(ms);
+                    bitmapImage = new BitmapImage();
+                    bitmapImage.SetSource(randomAccessStream);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Get BitmapImage error!:" + e.Message);
+                bitmapImage = null;
+            }            
+            return bitmapImage;
+        }
+    }
+}
